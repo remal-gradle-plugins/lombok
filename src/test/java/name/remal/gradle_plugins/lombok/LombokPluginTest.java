5c51e10ef3f3486fc45a630cc5ed718d208c1002
Semyon Levin <levin.semen@gmail.com>
2024-12-12 16:38:56 -0800

    [release-later] Additional tests

--- src/test/java/name/remal/gradle_plugins/lombok/LombokPluginTest.java
+++ src/test/java/name/remal/gradle_plugins/lombok/LombokPluginTest.java
@@ -1,9 +1,13 @@
 package name.remal.gradle_plugins.lombok;
 
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.packageNameOf;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.unwrapGeneratedSubclass;
+import static name.remal.gradle_plugins.toolkit.testkit.ProjectValidations.executeAfterEvaluateActions;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import name.remal.gradle_plugins.toolkit.testkit.TaskValidations;
 import org.gradle.api.Project;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +27,22 @@ class LombokPluginTest {
         val pluginManager = project.getPluginManager();
         assertDoesNotThrow(() -> pluginManager.apply(LombokPlugin.class));
         assertDoesNotThrow(() -> pluginManager.apply("java"));
+    }
+
+    @Test
+    void pluginTasksDoNotHavePropertyProblems() {
+        project.getPluginManager().apply(LombokPlugin.class);
+        project.getPluginManager().apply("java");
+
+        executeAfterEvaluateActions(project);
+
+        val taskClassNamePrefix = packageNameOf(LombokPlugin.class) + '.';
+        project.getTasks().stream()
+            .filter(task -> {
+                val taskClass = unwrapGeneratedSubclass(task.getClass());
+                return taskClass.getName().startsWith(taskClassNamePrefix);
+            })
+            .forEach(TaskValidations::assertNoTaskPropertiesProblems);
     }
 
 }
